aeb81db0ae5d77bedcb4ab181ad7f764e4a1e935
Jorge Ramirez <jorgeisc95@hotmail.com>
2021-09-20 23:06:55 -0500

    Avance notas

--- codigo.js
+++ codigo.js
@@ -1,7 +1,31 @@
-let cadena = "cadena de prueba\n"
-let cad= "cade"
-let resultado;
+let español=["español","Jorge","Cofla","Raul","Carlos"];
+let ingles=["ingles","Pedro","Pablo","Cofla","Raul"];
+let aleman=["aleman","Sara","Karla","Pable","Raul"];
 
-resultado=cadena.concat(cad);
-resultado=cadena.startsWith(cad);
-document.write(resultado);
+function materia(nombreMateria){
+    document.write(`El profesor es: ${nombreMateria[1]} <br>`);
+    let subcadena=nombreMateria.slice(2);
+    document.write(`Los Alumnos son: ${subcadena.join()} <br>`);
+}
+
+function cuantas(){
+    let cont=0;
+    if (español.includes("Cofla")){
+        cont++;
+        document.write(`Cofla esta en esta clase de español y el profesor es ${español[1]} <br>`)
+
+    }
+    if(ingles.includes("Cofla")){
+        cont++
+        document.write(`Cofla esta en esta clase de ingles y el profesor es ${ingles[1]} <br>`)
+    }
+    if(aleman.includes("Cofla")){
+        cont++;
+        document.write(`Cofla esta en esta clase de aleman y el profesor es ${aleman[1]}`)
+    }
+
+    document.write(`Cofla esta en ${cont} clases`)
+}
+
+materia(aleman);
+cuantas();
